1cddaecdd4d04b9df744525c288541f3c133eb80
Hasan Sadikin <hasanmcmissile2711@gmail.com>
2021-01-25 21:30:05 +0800

    deploy v4

---
+++ js/chunk-2d0e95df.981bff20.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"text-center pa-15 display-5"},[e._v(" Page Not Found ")])},c=[],s={},l=s,u=n("2877"),d=Object(u["a"])(l,a,c,!1,null,null,null);t["default"]=d.exports}}]);
+//# sourceMappingURL=chunk-2d0e95df.981bff20.js.map
